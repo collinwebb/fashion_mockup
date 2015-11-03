a479b64a78d234565ff572f2f064f28a72af2b8d
Collin Webb <collindwebb@gmail.com>
2015-11-03 15:04:23 -0800

    adds test view and urls

--- fashion_mockup/urls.py
+++ fashion_mockup/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^discover/', include('discover.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
