84e733746c32a026b5bd654f8cd02630d6e76478
Collin Webb <collindwebb@gmail.com>
2015-11-03 12:52:21 -0800

    adds product class in models

--- discover/models.py
+++ discover/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    blurb = models.CharField(max_length=200)
+    author = models.CharField(max_length=50)
+    thumbnail_url = models.CharField()
+    details_url = models.CharField()
