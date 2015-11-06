3b68967fe6745c16a35a48c56973de206874288a
Collin Webb <collindwebb@gmail.com>
2015-11-06 12:56:17 -0800

    creates test python view and index

--- discover/views.py
+++ discover/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from .models import Product
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the fashion discovery index.")
+    product_list = Product.objects.all()
+    output = ', '.join([p.title for p in product_list])
+    template = loader.get_template('discover/index.html')
+    context = RequestContext(request, {
+        'product_list': product_list,
+    })
+    return HttpResponse(template.render(context))
