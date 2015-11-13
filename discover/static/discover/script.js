4bdb11284c0d4360f946ded79ade5834a5344601
Collin Webb <collindwebb@gmail.com>
2015-11-13 15:47:56 -0800

    changes pageloads to infinite scrolling

--- discover/static/discover/script.js
+++ discover/static/discover/script.js
@@ -1,66 +1,54 @@
 var DiscoverFashion = React.createClass({
   getInitialState: function() {
-    return { page: 0, number_of_products: 1,
-      list: [
-        {fields: {
-          title: '',
-          blurb: '',
-          author: '',
-          thumbnail_url: '',
-          details_url: ''
-        }, pk: 1, model: "discover.product"}
-    ]};
+    return {
+      doneLoading: true,
+      listNotComplete: true,
+      page: 0,
+      list: []
+    };
+  },
+
+  infiniteScroll: function(){
+    var $r = this;
+
+    $(window).scroll(function () {
+      if ($(window).scrollTop() >= $(document).height() - $(window).height() - 100) {
+        // to prevent excess loading:
+        if ($r.state.listNotComplete && $r.state.doneLoading) {
+          $r.state.doneLoading = false;
+          $r.state.page += 1;
+          $r.componentDidMount();
+        }
+      }
+    });
   },
 
   componentDidMount: function() {
     $.get("json_data/" + this.state.page, function(result) {
-      console.log(result);
+      var anythingLoaded = result.length > 0;
+
       if (this.isMounted()) {
         this.setState({
-          page: this.state.page, number_of_products: result.length,
-          list: result
+          doneLoading: true,
+          listNotComplete: anythingLoaded,
+          page: this.state.page,
+          list: this.state.list.concat(result)
         });
       }
     }.bind(this));
-
-    var blankLastPage = this.state.list.length === 0;
-    var notFirstPage = this.state.page > 0
-    if (blankLastPage && notFirstPage){
-      this.state.page -= 1;
-      this.componentDidMount();
-    }
-  },
-
-  handleClick: function(direction) {
-    var onFirstPageGoPrev = this.state.page === 0 && direction === -1;
-    var onLastPageGoNext = this.state.number_of_products < 20 && direction === 1; //temp
-    if (onFirstPageGoPrev || onLastPageGoNext){
-      direction = 0;
-    }
-
-    this.state.page += direction;
-    this.componentDidMount();
+    this.infiniteScroll();
   },
 
   render: function() {
-    var rowStart, rowEnd, slice, rows = [];
-    for (var i = 0; i <= 4; i++) {
+    var rowStart, rowEnd, row, rows = [];
+    for (var i = 0; i <= this.state.list.length; i++) {
       rowStart = i * 4;
       rowEnd = ((i + 1) * 4);
-      slice = this.state.list.slice(rowStart, rowEnd);
-      rows.push(<CreateRow list={slice} key={i}/>);
+      row = this.state.list.slice(rowStart, rowEnd);
+      rows.push(<CreateRow list={row} key={i}/>);
     }
-
     return (
-      <div>
-        {rows}
-        <nav>
-          <ul className="pager">
-            <li><a onClick={this.handleClick.bind(this, -1)}>Previous</a></li>
-            <li><a onClick={this.handleClick.bind(this, 1)}>Next</a></li>
-          </ul>
-        </nav>
-      </div>
+      <div>{rows}</div>
     );
   }
 
@@ -88,7 +76,6 @@ var CreateRow = React.createClass({
     )
   }
 })
-
 
 ReactDOM.render(
   <DiscoverFashion />,
