a157eefad641faf35a72b207dd3f1109757ed156
Collin Webb <collindwebb@gmail.com>
2015-11-13 09:50:08 -0800

    creates single page loading with json

--- discover/urls.py
+++ discover/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^json_data$', views.json_data, name='json_data'),
 ]
--- discover/views.py
+++ discover/views.py
@@ -1,12 +1,15 @@
+import json
+from django.core import serializers
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from .models import Product
 
 def index(request):
-    product_list = Product.objects.all()
-    output = ', '.join([p.title for p in product_list])
     template = loader.get_template('discover/index.html')
-    context = RequestContext(request, {
-        'product_list': product_list,
-    })
+    context = RequestContext(request, {})
     return HttpResponse(template.render(context))
+
+
+def json_data(request):
+    data = serializers.serialize('json', Product.objects.all()[0:20])
+    return HttpResponse(data, content_type='application/json')
